56e78ecca9febbd4175c90bc693a1bcf94b264af
Niko Boehm <coding@semisane.de>
2016-04-04 22:35:26 +0200

    added MultiHandler with double and log clicks

--- buzz.py
+++ buzz.py
@@ -3,39 +3,128 @@
 import RPi.GPIO as GPIO
 import time
 import os
+import traceback
 
 #LED=11
-Buttons=[12, 16, 18]
+num_Buttons = 3
+pressed_level = 0
+Buttons = [12, 16, 18]
 
 def setup():
     GPIO.setmode(GPIO.BOARD)
     for btn in Buttons:
         GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def loop():
-    pressed = 0
-    nochange = 0
-    while True:
-        b = 0
-        m = 1
 
-        # scan for pressed buttons
-        for i in range(0,3):
-            if GPIO.input(Buttons[i]) == 1:
-                b = b | m
-            m = m << 1
+def get_buttons():
+    b = 0
+    m = 1
+    # scan for pressed buttons
+    for i in range(0,num_Buttons):
+        if GPIO.input(Buttons[i]) == pressed_level:
+            b = b | m
+        m = m << 1
+    return b
 
-        # check for change
-        if b != pressed:
-            print "Button change! old: %d\tnew: %d\tnot changed for: %d" % (pressed, b, nochange)
-            pressed = b
-            nochange = 0
+
+class SimpleHandler:
+    def __init__(self):
+        self.prev = 0
+        self.hold = 0
+
+    def parse(self, btn):
+        # count same cycles (for de-bouncing)
+        if self.prev == btn:
+            self.hold = self.hold + 1
         else:
-            nochange = nochange + 1
+            self.hold = 0
+
+        r = 0
+        if self.hold == 0:
+            r = btn
+
+        self.prev = btn
+        return r
+
+
+class MultiHandler:
+    def __init__(self):
+        self.pressed1 = 0
+        self.pressed2 = 0
+        self.prev = 0
+        self.hold = 0
+        self.pressedLong = 0
+        self.bstates = (1 << num_Buttons)-1 
+    
+    def action(self):
+        a = 0
+        if self.pressed2 == 0:
+            a = self.pressed1
+            if self.pressedLong:
+                a = a + self.bstates**2 + self.bstates
+        else:
+            a = (self.bstates * self.pressed2) + self.pressed1
+        print "pressed1 = %d, pressed2 = %d, pressedLong = %s --> action = %d" % (self.pressed1, self.pressed2, self.pressedLong, a)
+        self.pressed1 = 0
+        self.pressed2 = 0
+        return a
+
+
+    def parse(self, btn):
+        # count same cycles (for de-bouncing)
+        if self.prev == btn:
+            self.hold = self.hold + 1
+        else:
+            self.hold = 0
+
+        if self.pressedLong:
+            # print "state C - long pressed; waiting for release"
+            if btn == 0:
+                self.pressedLong = False
+        elif self.pressed1 == 0:
+            # print "state A - nothing pressed so far"
+            if self.hold == 1:
+                # trans to B
+                self.pressed1 = btn
+        elif self.pressed2 != 0:
+            # print "state E - double click; waiting for release"
+            if btn == 0:
+                return self.action()
+            if self.hold == 1:
+                # update buttons
+                self.pressed2 = btn
+        else:
+            if self.prev == 0:
+                # print "state D - released; waiting for double"
+                if self.hold == 3:
+                    return self.action()
+                elif btn != 0:  
+                    # trans to E
+                    self.pressed2 = btn
+            else:
+                # print "state B - first press"
+                if btn != 0 and self.hold == 1:
+                    # update buttons
+                    self.pressed1 = btn
+                if self.hold == 20:
+                    # trans to C
+                    self.pressedLong = True
+                    return self.action()
+
+        self.prev = btn
+        return 0
+
+
+def loop():
+    # top level loop
+    #handler = SimpleHandler()
+    handler = MultiHandler()
+    while True:
+        b = get_buttons()
+        action = handler.parse(b)
        
-        # if buttons are stable for one tick: play sound
-        if (nochange == 1) and (pressed != 0):
-            os.system('mpc play %d' % (pressed))
+        if action != 0:
+            os.system('mpc play %d' % (action))
 
         # tick
         time.sleep(0.05)
@@ -44,7 +133,7 @@ setup()
 try:
     loop()
 except:
-    pass
+    traceback.print_exc()
 GPIO.cleanup()
 
 
